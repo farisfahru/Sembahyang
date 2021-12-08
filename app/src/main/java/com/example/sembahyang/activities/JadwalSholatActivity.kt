7df9a905c76c8db837e92360eed8df3107ccc771
nraliim <x.androtube@gmail.com>
2021-12-08 21:23:38 +0700

    menambahkan pengaturan tanggal jadwal sholat

--- app/src/main/java/com/example/sembahyang/activities/JadwalSholatActivity.kt
+++ app/src/main/java/com/example/sembahyang/activities/JadwalSholatActivity.kt
@@ -7,25 +7,36 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sembahyang.R
 import com.example.sembahyang.databinding.ActivityJadwalSholatBinding
 import com.example.sembahyang.model.ModelKota
 import com.example.sembahyang.utils.ClientAsyncTask
+import com.example.sembahyang.utils.DatePickerFragment
 import org.json.JSONException
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 
-class JadwalSholatActivity : AppCompatActivity() {
+class JadwalSholatActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragment.DialogDateListener {
 
     private lateinit var binding: ActivityJadwalSholatBinding
     private lateinit var listModelKota: MutableList<ModelKota>
     private lateinit var modelKotaAdapter: ArrayAdapter<ModelKota>
     private lateinit var progressDialog: ProgressDialog
 
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityJadwalSholatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        binding.tvDate.text = currentDate.format(Date())
+
+        binding.btnDate.setOnClickListener(this)
 
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Mohon Tunggu")
@@ -33,6 +44,36 @@ class JadwalSholatActivity : AppCompatActivity() {
         progressDialog.setMessage("Sedang menampilkan jadwal...")
 
         //show data spinner
+        showDataSpinner()
+
+        //get data kota
+        getDataKota()
+
+        return
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_date -> {
+                val datePickerFragment = DatePickerFragment()
+                datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
+            }
+        }
+
+    }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+        binding.tvDate.text = dateFormat.format(calendar.time)
+        showDataSpinner()
+        getDataKota()
+    }
+
+    private fun showDataSpinner() {
         val spKota: Spinner = binding.spinCity
         listModelKota = ArrayList()
         modelKotaAdapter = ArrayAdapter(
@@ -50,20 +91,14 @@ class JadwalSholatActivity : AppCompatActivity() {
                 getDataJadwal(spinKota?.id)
             }
         }
-
-        //get data kota
-        getDataKota()
-
-        return
-
     }
 
     private fun getDataJadwal(id: Int?) {
         try {
             progressDialog.show()
             val idKota = id.toString()
-            val current = SimpleDateFormat("yyyy-MM-dd")
-            val tanggal = current.format(Date())
+            val tvDate = binding.tvDate.text.toString()
+            val tanggal = tvDate.format(Date())
             val url = "https://api.banghasan.com/sholat/format/json/jadwal/kota/$idKota/tanggal/$tanggal"
             val task = ClientAsyncTask(this, object : ClientAsyncTask.OnPostExecuteListener {
                 override fun onPostExecute(result: String) {
@@ -91,12 +126,10 @@ class JadwalSholatActivity : AppCompatActivity() {
 
     private fun getDataKota() {
         try {
-//            progressDialog.show()
             val url = "https://api.banghasan.com/sholat/format/json/kota"
             val task = ClientAsyncTask(this, object : ClientAsyncTask.OnPostExecuteListener {
                 override fun onPostExecute(result: String) {
                     try {
-//                        progressDialog.dismiss()
                         val jsonObject = JSONObject(result)
                         val jsonArray = jsonObject.getJSONArray("kota")
                         var daftarKota: ModelKota?
